f76e6732d552781b8089fccc1bef14bd203c34f0
gantz25 <61122330+gantz25@users.noreply.github.com>
2022-10-04 00:55:15 +0200

    Add files via upload

---
+++ Chapter 2/CAMA2PP2/main.c
@@ -0,0 +1,19 @@
+/* Book name : C a modern approach
+*  Chapter   : 2
+*  Programming Project :1
+*  Name      : CAMA2PP2.c (c a modern approach chapter 2 example 2 and 3
+*  Author    : Mohammed Ali
+*  purpose   : Writing a program to calculate the volume of a sphere of 10 M R
+*/
+# include <stdio.h>
+# define   PI  3.14
+int main(voi)
+{
+    int raduis;
+    printf("**********\n* Hello , this is a program to calculate the volume of a sphere\n**********\n");
+    printf("enter the raduis\n");
+    scanf("%d",&raduis);
+    printf("volume is : %f",4.0f/3.0f*PI*raduis*raduis*raduis);
+
+    return 0;
+}
